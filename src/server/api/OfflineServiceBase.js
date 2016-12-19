45adfd3c60b7f8c50b020b2c5b39d552c56ca579
Vishal Jariwala <vdjariwala@gmail.com>
2016-12-19 15:55:57 -0600

    added jsong for entity attributes added offline mode for get and search api added node.js api solution

---
+++ src/server/api/OfflineServiceBase.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var OfflineServiceBase = function(options) {
+        //console.log('Offline service instance initiated with settings', JSON.stringify({options: options}, null, 4));
+};
+
+module.exports = OfflineServiceBase;
+
+
+
+
+
+
+
