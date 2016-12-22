37fc5dea5c02921eaea284e21f8c6c30c4026d39
Vishal Jariwala <vdjariwala@gmail.com>
2016-12-22 12:30:29 -0600

    formatted code. updated rdp conn conf

--- src/server/api/EntityManageService/DPEntityManageService.js
+++ src/server/api/EntityManageService/DPEntityManageService.js
@@ -2,27 +2,18 @@
 
 var DPServiceBase = require('../DPServiceBase');
 
-var DPEntityManageService = function(options) {
+var DPEntityManageService = function (options) {
     DPServiceBase.call(this, options);
 };
 
 DPEntityManageService.prototype = {
-    getEntities: async function(request){
-        var url = this.baseUrl + '/entityManageService/get';
-        
-        var options = {
-            url: url,
-            method: "POST",
-            headers:{
-                //"Content-type": "application/json",
-                "Cache-Control": "no-cache"
-            },
-            body: request,
-            json: true
-        };
-
-        var res = await this.client(options);
-        return res;
+    getEntities: async function (request) {
+        var url = '/entityManageService/get';
+        return await this.requestJson(url, request);
+    },
+    saveEntities: async function (request) {
+        var url = '/entityManageService/set';
+        return await this.requestJson(url, request);
     }
 };
 
