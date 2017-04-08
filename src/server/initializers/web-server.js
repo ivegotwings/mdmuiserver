f0affe703d8e8b50f54cba4c235399c649f2fbba
Nilesh <nilesh.prajapati@outlook.com>
2017-04-08 23:40:59 +0530

    Few auth token/key related config changes as per change in auth service

--- src/server/initializers/web-server.js
+++ src/server/initializers/web-server.js
@@ -43,6 +43,8 @@ app.get('/', function (req, res) {
     var lastName = req.header("x-rdp-lastName");
     var userEmail = req.header("x-rdp-userEmail");
     if (tenantId && userId) {
+        res.append("x-rdp-userId", userId);
+        res.append("x-rdp-userRoles", userRoles);
         res.render('index', { isAuthenticated: true, tenantId: tenantId, userId: userId, roleId: userRoles });
     }
     else {
@@ -82,6 +84,8 @@ app.get('*', function (req, res) {
     var lastName = req.header("x-rdp-lastName");
     var userEmail = req.header("x-rdp-userEmail");
     if (tenantId && tenantId != "" && userId && userId != "") {
+        res.append("x-rdp-userId", userId);
+        res.append("x-rdp-userRoles", userRoles);
         res.render('index', { isAuthenticated: true, tenantId: tenantId, userId: userId, roleId: userRoles });
     }
     else {
