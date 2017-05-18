7e89889d728925d40feef2c7fa0101088ed8a905
Jay Kakadiya <jay.patel2@riversand.com>
2017-05-18 13:17:50 +0530

    File upload and download changes for directory. Added config to set which directory will contain download and upload folder.

--- src/server/modules/file-download/file-download-route.js
+++ src/server/modules/file-download/file-download-route.js
@@ -1,23 +1,31 @@
 'use strict';
+var config = require('../../config/rdf-connection-config.json');
+var isEmpty = require('../common/utils/isEmpty');
 
 
 var fs = require('fs');
 
-module.exports = function(app) {
+module.exports = function (app) {
     var dir = './download';
-    
+
+    if (config && !isEmpty(config.fileStoragePath)) {
+        if (fs.existsSync(config.fileStoragePath)) {
+            dir = config.fileStoragePath + '/download';
+        }
+    }
+
     if (!fs.existsSync(dir)) {
         fs.mkdirSync(dir);
     }
 
     app.get('/file-download', function (req, res) {
-            if (!req.query || !req.query["fileName"]) {
-                res.send('File name is not there for download.');
-                return;
-            }
-
-            var fileName = req.query["fileName"];
-            res.download(dir + '/'+ fileName);
+        if (!req.query || !req.query["fileName"]) {
+            res.send('File name is not there for download.');
+            return;
         }
+
+        var fileName = req.query["fileName"];
+        res.download(dir + '/' + fileName);
+    }
     );
 };
