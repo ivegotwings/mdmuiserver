9d18039cff2e9bd135e02e2a84cb2894c2aaf750
Ravi Kumar. Dhanasekaran <ravi.kumar@riversand.com>
2017-09-05 18:37:12 +0530

    Polymer2 - Gulp task issues fixed.

--- gulpfile.js
+++ gulpfile.js
@@ -94,8 +94,8 @@ function devBuild() {
     let dependenciesStreamSplitter = new polymerBuild.HtmlSplitter();
 
     // Okay, so first thing we do is clear the build directory
-    console.log(`Deleting ${buildDirectory} directory...`);
-    del([buildDirectory])
+    console.log(`Deleting ${devPath} directory...`);
+    del([devPath])
       .then(() => {
 
         // Let's start by getting your source files. These are all the files
@@ -268,4 +268,73 @@ gulp.task('copy-node-modules', function () {
 
 gulp.task('dev', gulp.series(devBuild, 'app-nodemon', 'watch-element-changes'));
 
-gulp.task('default', gulp.series('copy-node-modules'));
+function unbundledBuild() {
+  return new Promise((resolve, reject) => { // eslint-disable-line no-unused-vars
+
+    // Lets create some inline code splitters in case you need them later in your build.
+    let sourcesStreamSplitter = new polymerBuild.HtmlSplitter();
+    let dependenciesStreamSplitter = new polymerBuild.HtmlSplitter();
+
+    // Okay, so first thing we do is clear the build directory
+    console.log(`Deleting ${unbundledPath} directory...`);
+    del([unbundledPath])
+      .then(() => {
+
+        // Let's start by getting your source files. These are all the files
+        // in your `src/` directory, or those that match your polymer.json
+        // "sources"  property if you provided one.
+        let sourcesStream = polymerProject.sources()
+
+          // If you want to optimize, minify, compile, or otherwise process
+          // any of your source code for production, you can do so here before
+          // merging your sources and dependencies together.
+        //   .pipe(gulpif(/\.(png|gif|jpg|svg)$/, imagemin()))
+
+          // The `sourcesStreamSplitter` created above can be added here to
+          // pull any inline styles and scripts out of their HTML files and
+          // into seperate CSS and JS files in the build stream. Just be sure
+          // to rejoin those files with the `.rejoin()` method when you're done.
+          .pipe(sourcesStreamSplitter.split())
+          // Uncomment these lines to add a few more example optimizations to your
+          // source files, but these are not included by default. For installation, see
+          // the require statements at the beginning.
+          // .pipe(gulpif(/\.js$/, uglify())) // Install gulp-uglify to use
+          // .pipe(gulpif(/\.css$/, cssSlam())) // Install css-slam to use
+          // .pipe(gulpif(/\.html$/, htmlMinifier())) // Install gulp-html-minifier to use
+          //.pipe(gulpif('**/*.js', babel()))
+          // Remember, you need to rejoin any split inline code when you're done.
+          .pipe(sourcesStreamSplitter.rejoin());
+          //.pipe(debug({title:"After join"}));
+
+
+        // Similarly, you can get your dependencies seperately and perform
+        // any dependency-only optimizations here as well.
+        let dependenciesStream = polymerProject.dependencies()
+          .pipe(dependenciesStreamSplitter.split())
+          // Add any dependency optimizations here.
+          .pipe(dependenciesStreamSplitter.rejoin());
+
+
+        // Okay, now let's merge your sources & dependencies together into a single build stream.
+        let buildStream = mergeStream(sourcesStream, dependenciesStream)
+          .once('data', () => {
+            console.log('Analyzing build dependencies...');
+          });
+
+        // Now let's generate the HTTP/2 Push Manifest
+        buildStream = buildStream.pipe(polymerProject.addPushManifest());
+
+        // Okay, time to pipe to the build directory
+        buildStream = buildStream.pipe(gulp.dest(unbundledPath));//.pipe(debug({title:"Copy:"}));;
+
+        // waitFor the buildStream to complete
+        return waitFor(buildStream);
+      })
+      .then(() => {
+        // You did it!
+        console.log('Build complete!');
+        resolve();
+      });
+  });
+}
+gulp.task('default', gulp.series(unbundledBuild, 'copy-node-modules'));
