42a68e4732a319cc766f7492182618aa91b7f5bc
Shafil <shafil.chekkiyattu@riversand.com>
2017-10-24 18:04:15 +0530

    Update notification-manager.js

--- src/server/modules/notification-engine/api/notification-manager.js
+++ src/server/modules/notification-engine/api/notification-manager.js
@@ -5,11 +5,11 @@ var logger = require('../../common/logger/logger-service');
 function sendMessageToAllUser(data) {
         if(socketManager.sendMessage) {  
 
-                logger.debug("Sending notification to user", {detail: {notificationObj:data}}, "notification-service");
+                logger.debug("Sending notification to all user", {detail: {notificationObj:data}}, "notification-service");
                 socketManager.sendMessage(data);
         }
         else {
-                logger.info('server socket is not available');
+                logger.info('Server socket is not available');
         }
 }
 
@@ -20,7 +20,7 @@ function sendMessageToSpecificUser(data, userId) {
                 socketManager.sendMessage(data, userId);
         }
         else {
-                logger.info('server socket is not available');
+                logger.info('Server socket is not available');
         }
 }
 
