cddf498302597b84376ec512a36865ec0f8d5053
Vishal Jariwala <vishaljariwala@users.noreply.github.com>
2017-10-26 11:01:32 +0530

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -103,7 +103,7 @@ gulp.task('build-main', function() {
         .pipe(gulp.dest(mainPath + "/src/static/es5"))
       polymerBuild.addServiceWorker({
           project: polymerProject,
-          buildRoot: devPath + "/src/static/es5",
+          buildRoot: mainPath + "/src/static/es5",
           bundled: false,
           swPrecacheConfig: swPrecacheConfig
       });
