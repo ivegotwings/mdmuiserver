48e7e85d255d2fa3bb1ef7d1a330a29a21499378
Shafil <shafil.chekkiyattu@riversand.com>
2017-12-05 12:52:13 +0530

    Update web-engine-loader.js

--- src/server/modules/web-engine/web-engine-loader.js
+++ src/server/modules/web-engine/web-engine-loader.js
@@ -213,7 +213,7 @@ function renderAuthenticatedPage(req, res) {
         if (fullName == "") {
             fullName = userId;
         }
-        res.render('index', { isAuthenticated: true, tenantId: tenantId, userId: userId, roleId: userRoles, fullName: fullName, userName: userName, ownershipData: ownershipData, noPreload: true });
+        res.render('index', { isAuthenticated: true, tenantId: tenantId, userId: userId, roleId: userRoles, fullName: fullName, userName: userName, ownershipData: ownershipData, noPreload: false });
         return true;
     } else {
         return false;
