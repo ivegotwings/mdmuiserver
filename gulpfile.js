ee43bda5718bb5f164195d4f2b967e5a945f4c48
Vishal Jariwala <jariwalav@rsus-vishalmbpriversandcom.local>
2017-12-19 15:41:30 +0530

    added static root for the debug route....added uglify in gulp(from Shiv)

--- gulpfile.js
+++ gulpfile.js
@@ -17,6 +17,7 @@ const gulp = require('gulp');
 const gulpif = require('gulp-if');
 const gulpnoop = require('gulp-noop');
 const debug = require('gulp-debug');
+const uglify = require('gulp-uglify-es').default;
 
 const nodemon = require('gulp-nodemon');
 var rename = require("gulp-rename");
@@ -33,6 +34,7 @@ const polymerBuild = require('polymer-build');
 const forkStream = require('polymer-build').forkStream;
 
 const polyBundler = require('polymer-bundler');
+const crisper = require('gulp-crisper');
 
 // Additional plugins can be used to optimize your source files after splitting.
 // Before using each plugin, install with `npm i --save-dev <package-name>`
@@ -50,8 +52,35 @@ const serverPath = path.join(buildDirectory, 'web-server');
 //const nginxDirectory = '/usr/local/etc/nginx';
 const es6BundledPath = 'static/es6-bundled';
 const es5BundledPath = 'static/es5-bundled';
+const es6unBundledPath = 'static/es6-unbundled';
 
 const liveReloadPort = 35729;
+
+let uglifyOptions ={
+  parse: {
+      ecma:6
+  },
+  compress: {
+    ecma:6,
+    drop_console: true,
+    passes: 3,
+    toplevel:false,
+    top_retain: false,
+    unsafe: false
+      // compress options
+  },
+  mangle:{
+    toplevel:false,
+  },
+  ecma: 6, // specify one of: 5, 6, 7 or 8
+  keep_classnames: false,
+  keep_fnames: false,
+  ie8: false,
+  nameCache: null, // or specify a name cache object
+  safari10: false,
+  toplevel: false,
+  warnings: false,
+};
 
 function waitFor(stream) {
   return new Promise((resolve, reject) => {
@@ -78,16 +107,6 @@ function serverBuild(buildPath) {
     resolve();
   });
 }
-
-/*function clientBuild(relativeBuildPath, bundle, isES5) {
-  return new Promise((resolve, reject) => {
-    console.log(`BuildPath : ${buildPath} ...`);
-    var serverSources = polymerJson.serverSources;
-    gulp.src(serverSources, { base: '.' }).pipe(gulp.dest(buildPath));
-    //console.log(`Completed : ${buildPath} !`);
-    resolve();
-  });
-}*/
 
 function clientBuild(relativeBuildPath, bundle, isES5) {
   return new Promise((resolve, reject) => {
@@ -117,14 +136,24 @@ function clientBuild(relativeBuildPath, bundle, isES5) {
         dependenciesStream = dependenciesStream.pipe(gulpif(['**/*.js', "!bower_components/pdfjs-dist/**/*.js", "!bower_components/mocha/mocha.js", "!bower_components/jsoneditor/dist/jsoneditor.min.js", "!bower_components/resize-observer-polyfill/**/*.js"], babel({'presets': [['es2015', {'modules': false, 'compact': false, 'allowReturnOutsideFunction': true}]]})));
       }
 
-      sourcesStream = sourcesStream
-        //.pipe(gulpif("**/*.css", cssSlam()))
-      .pipe(gulpif("**/*.html", minifyHTML()));        
+      if(bundle) {
+        sourcesStream = sourcesStream
+          //.pipe(gulpif("**/*.css", cssSlam()))
+          .pipe(gulpif("**/*.html", minifyHTML()))
+          .pipe(gulpif("**/*.js", (babel({
+              plugins: ["transform-class-properties"]
+            }))))
+          .pipe(gulpif("**/*.js", uglify(uglifyOptions)))    
 
-      dependenciesStream = dependenciesStream
-        //.pipe(gulpif("**/*.css", cssSlam()))
-        .pipe(gulpif("**/*.html", minifyHTML()));
-
+        dependenciesStream = dependenciesStream
+          //.pipe(gulpif("**/*.css", cssSlam()))
+          .pipe(gulpif("**/*.html", minifyHTML()))
+          .pipe(gulpif("**/*.js", (babel({
+              plugins: ["transform-class-properties"]
+            }))))
+          .pipe(gulpif("**/*.js", uglify(uglifyOptions)))
+      }
+      
       sourcesStream = sourcesStream.pipe(sourcesStreamSplitter.rejoin());
       dependenciesStream = dependenciesStream.pipe(dependenciesStreamSplitter.rejoin());
       
@@ -139,11 +168,11 @@ function clientBuild(relativeBuildPath, bundle, isES5) {
         buildStream = buildStream
           .pipe(polymerProject.bundler({
             excludes: bundleExcludes,
-            inlineScripts: false,
-            inlineCss: false,
+            inlineScripts: true,
+            inlineCss: true,
             rewriteUrlsInTemplates: false,
             sourcemaps: false,
-            stripComments: true, //, //,
+            stripComments: true,
             // Merge shared dependencies into a single bundle when
             // they have at least three dependents
             strategy: generateShellOnlyMergeStrategy(polymerJson.shell, 3),
@@ -152,61 +181,46 @@ function clientBuild(relativeBuildPath, bundle, isES5) {
             urlMapper: polyBundler.generateCountingSharedBundleUrlMapper('src/shared-bundles/bundle_')
           }))
           .once('data', () => { console.log('Bundling resources... '); });
+
+        waitFor(buildStream);
+
+        buildStream = buildStream.pipe(gulpif("**/*.html", crisper({
+          scriptInHead: false, // true is default 
+          onlySplit: false
+        })));
       }
 
       // Now let's generate the HTTP/2 Push Manifest
-      buildStream = buildStream.pipe(polymerProject.addPushManifest());
-
-      // let nginxStream = forkStream(buildStream);
-      // nginxStream = nginxStream.pipe(gulp.dest(nginxPath));
-      // waitFor(nginxStream);
+      //buildStream = buildStream.pipe(polymerProject.addPushManifest());
 
       buildStream = buildStream.pipe(gulp.dest(buildPath));
 
       // let build stream finish his work..
       return waitFor(buildStream).then(function (){
-              console.log('Generating the service worker... ');
+              // console.log('Generating the service worker... ');
 
               // /*gulp task to settimeout all script tags*/
-              //  gulp.src("./build/ui-platform/static/es6-bundled/src/elements/*/*.html", "./build/ui-platform/static/es6-bundled/src/shared-bundles/") 
+              //  gulp.src(["./build/ui-platform/static/es6-bundled/src/elements/*/*.html"]) 
               //  .pipe(replace(/<script>([\s\S]*?)<\/script>/gm,
-              //       function(match, p1, offset, string) {
-              //       if(match.indexOf("behavior") !== -1) return match;
-              //       return "<script>" + "setTimeout("+match.substring(8,match.length-9)+",0.5)" + "</script>";
+              //       function(match, p1, offset, string) { 
+              //       if(match.indexOf("saulis") !== -1 || match.indexOf("RUFBehaviors.DataChannel") !== -1 || match.indexOf("window.RUFBehaviors") !== -1 || match.indexOf("window.Polymer") !== -1){
+              //         return match;                                        
+              //       }
+              //       return "<script>" + "setTimeout("+"(function(){" + match.substring(8,match.length-9)+ "})(),0.5)" + "</script>";;  
               //     }
               //   ))
               //  .pipe(gulp.dest('./build/ui-platform/static/es6-bundled/src/elements'));
 
-        // return polymerBuild.addServiceWorker({
-        //   project: polymerProject,
-        //   buildRoot: buildPath,
-        //   bundled: bundle,
-        //   swPrecacheConfig: swPrecacheConfig
-        // });
-
-
-              /*gulp task to settimeout all script tags*/
-               gulp.src(["./build/ui-platform/static/es6-bundled/src/elements/*/*.html"]) 
-               .pipe(replace(/<script>([\s\S]*?)<\/script>/gm,
-                    function(match, p1, offset, string) { 
-                    if(match.indexOf("saulis") !== -1 || match.indexOf("RUFBehaviors.DataChannel") !== -1 || match.indexOf("window.RUFBehaviors") !== -1 || match.indexOf("window.Polymer") !== -1){
-                      return match;                                        
-                    }
-                    return "<script>" + "setTimeout("+"(function(){" + match.substring(8,match.length-9)+ "})(),0.5)" + "</script>";;  
-                  }
-                ))
-               .pipe(gulp.dest('./build/ui-platform/static/es6-bundled/src/elements'));
-
-               gulp.src(["./build/ui-platform/static/es6-bundled/src/shared-bundles/*.html"]) 
-               .pipe(replace(/<script>([\s\S]*?)<\/script>/gm,
-                    function(match, p1, offset, string) {
-                   if(match.indexOf("saulis") !== -1 || match.indexOf("RUFBehaviors.DataChannel") !== -1 || match.indexOf("window.RUFBehaviors") !== -1 || match.indexOf("window.Polymer") !== -1){
-                    return match;                                        
-                   }
-                    return "<script>" + "setTimeout("+"(function(){" + match.substring(8,match.length-9)+ "})(),0.5)" + "</script>";;  
-                  }
-                ))
-               .pipe(gulp.dest('./build/ui-platform/static/es6-bundled/src/shared-bundles'));
+              //  gulp.src(["./build/ui-platform/static/es6-bundled/src/shared-bundles/*.html"]) 
+              //  .pipe(replace(/<script>([\s\S]*?)<\/script>/gm,
+              //       function(match, p1, offset, string) {
+              //      if(match.indexOf("saulis") !== -1 || match.indexOf("RUFBehaviors.DataChannel") !== -1 || match.indexOf("window.RUFBehaviors") !== -1 || match.indexOf("window.Polymer") !== -1){
+              //       return match;                                        
+              //      }
+              //       return "<script>" + "setTimeout("+"(function(){" + match.substring(8,match.length-9)+ "})(),0.5)" + "</script>";;  
+              //     }
+              //   ))
+              //  .pipe(gulp.dest('./build/ui-platform/static/es6-bundled/src/shared-bundles'));
           resolve();
       });
 
@@ -268,6 +282,12 @@ gulp.task('prod-es6-bundled-build', function () {
   ]);
 });
 
+gulp.task('prod-es6-unbundled-build', function () {
+  return Promise.all([
+    clientBuild(es6unBundledPath, false, false)
+  ]);
+});
+
 gulp.task('prod-es5-bundled-build', function () {
   return Promise.all([
     clientBuild(es5BundledPath, true, true)
@@ -287,8 +307,8 @@ gulp.task('copy-node-modules', function () {
   return gulp.src(nodeModulesPath, { base: '.' }).pipe(gulp.dest(unbundledPath));
 });
 
-gulp.task('prod-client-build', gulp.series('prod-es5-bundled-build', 'prod-es6-bundled-build'));
-gulp.task('default', gulp.series('prod-delete', 'prod-server-build', 'prod-es6-bundled-build', 'prod-build-wrap-up'));
+gulp.task('prod-client-build', gulp.series('prod-es5-bundled-build', 'prod-es6-bundled-build', 'prod-es6-unbundled-build'));
+gulp.task('default', gulp.series('prod-delete', 'prod-server-build', 'prod-es6-bundled-build', 'prod-es6-unbundled-build', 'prod-build-wrap-up'));
 
 //gulp.task('default', gulp.series('prod-delete', 'prod-server-build', 'prod-es5-bundled-build', 'prod-es6-bundled-build', 'prod-build-wrap-up'));
 
