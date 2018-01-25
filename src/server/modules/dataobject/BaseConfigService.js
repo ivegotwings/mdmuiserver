04bb1bfab8e99eba24796e8a7075b99ca6a2948a
Vishal Jariwala <jariwalav@rsus-vishalmbpriversandcom.local>
2018-01-25 14:56:21 +0530

    added cloning of local cache of base config to avoid live ref updates

--- src/server/modules/dataobject/BaseConfigService.js
+++ src/server/modules/dataobject/BaseConfigService.js
@@ -2,7 +2,9 @@
 
 var DFRestService = require('../common/df-rest-service/DFRestService');
 const fs = require('fs'),
-      path = require('path');
+    path = require('path');
+
+const falcorUtil = require('../../../shared/dataobject-falcor-util');
 
 var BaseConfigService = function (options) {
     DFRestService.call(this, options);
@@ -17,30 +19,34 @@ BaseConfigService.prototype = {
         var serviceConfig = SERVICE_CONFIG.services[url];
 
         var mode = "online";
-        if(serviceConfig && serviceConfig.baseConfigMode && serviceConfig.baseConfigMode == "offline"){
+        if (serviceConfig && serviceConfig.baseConfigMode && serviceConfig.baseConfigMode == "offline") {
             mode = "offline";
         }
         var fileId = baseConfigRequest.params.query.id;
-        if(localConfigCache[fileId]){
-            return localConfigCache[fileId];
+
+        if (localConfigCache[fileId]) {
+            return falcorUtil.cloneObject(localConfigCache[fileId]);
         }
+
         var baseConfigResponse;
-        if(mode == "offline"){
+        if (mode == "offline") {
             baseConfigResponse = await this._getOfflineBaseConfig(url, fileId);
-        }else{
+        } else {
             baseConfigResponse = await this.post(url, baseConfigRequest);
         }
-        localConfigCache[fileId] = baseConfigResponse;
+
+        localConfigCache[fileId] = falcorUtil.cloneObject(baseConfigResponse);
+
         return baseConfigResponse;
     },
-    _getOfflineBaseConfig: async function(url, fileId){
-        var fileName = fileId.replace("-base_uiConfig", "")+".json";
-        var baseConfigFilePath = path.join(process.cwd(), "../customer-seed-tenant-base/999-New-ui-config-WIP/00-base/"+fileName);
+    _getOfflineBaseConfig: async function (url, fileId) {
+        var fileName = fileId.replace("-base_uiConfig", "") + ".json";
+        var baseConfigFilePath = path.join(process.cwd(), "../customer-seed-tenant-base/999-New-ui-config-WIP/00-base/" + fileName);
         var file = require(baseConfigFilePath);
         var configObjects = file.configObjects ? file.configObjects : [];
         var resObj = {
-            response:{
-                "configObjects":configObjects
+            response: {
+                "configObjects": configObjects
             }
         };
         return resObj;
