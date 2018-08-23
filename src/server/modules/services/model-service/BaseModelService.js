f0ef4055d567a0a962c00726bf90018572e5e18c
nikhilreddy19 <nikhil.reddy@riversand.com>
2018-08-23 16:31:50 +0530

    Removed console.logs and small modification in while loop

--- src/server/modules/services/model-service/BaseModelService.js
+++ src/server/modules/services/model-service/BaseModelService.js
@@ -328,13 +328,11 @@ BaseModelService.prototype = {
             transformedModel = await this._transformCompositeModelForSave(compositeEntityTypeModel, request.entityModel);
 
             if (transformedModel) {
-                console.log("transformedModel:", JSON.stringify(transformedModel));
                 model = this._prepareEntityTypeModel(transformedModel);
                 compositeModels = this._prepareCompositeModels(transformedModel, compositeAttributeModel, request.entityModel);
             }
 
             if (!isEmpty(model)) {
-                console.log("Entity type Model:", JSON.stringify(model));
                 request[request.dataIndex] = model;
                 let dataOperationResult = await dataObjectManageService.process(request, action);
                 dataOperationResults.push(dataOperationResult);
@@ -353,7 +351,6 @@ BaseModelService.prototype = {
             }
 
             if (!isEmpty(compositeModels)) {
-                console.log("composite models:", JSON.stringify(compositeModels));
                 for (let compositeModel of compositeModels) {
                     request[request.dataIndex] = compositeModel;
                     let dataOperationResult = await dataObjectManageService.process(request, action);
@@ -539,7 +536,6 @@ BaseModelService.prototype = {
                     selfAttrsAndRels = await this._transformModelsIntoAttributesAndRelationships(compositeModelData, modelData);
 
                     if (selfAttrsAndRels) {
-                        console.log("self attributes and relationships:", JSON.stringify(selfAttrsAndRels));
                         if (selfAttrsAndRels.attributes) {
                             transformedModel.data.attributes = selfAttrsAndRels.attributes;
                         }
@@ -948,7 +944,6 @@ BaseModelService.prototype = {
                     case "haschildattributes":
                     case "hasrelationshipattributes": {
                         let attributes = entityTypeModelData.relationships[relType];
-                        console.log("attributes:", JSON.stringify(attributes));
                         let entityIds = attributes.map(v => v.relTo.id);
         
                         if (entityIds) {
@@ -960,11 +955,10 @@ BaseModelService.prototype = {
                                 if(isEmpty(entities)) {
                                     retryCount++;
                                 } else {
-                                    retryCount = maxRetryCount + 1;
+                                    break;
                                 }
                             }
                             if (entities) {
-                                console.log("attributeModel entities:", JSON.stringify(entities));
                                 for (let attr of attributes) {
                                     if (attr.action || attr.attributes) {
                                         let entity = entities.find(v => v.id == attr.relTo.id);
@@ -994,7 +988,6 @@ BaseModelService.prototype = {
                     }
                     case "hasrelationships": {
                         let relationships = entityTypeModelData.relationships[relType];
-                        console.log("relationships:", JSON.stringify(relationships));
                         let relEntityIds = relationships.map(v => v.relTo.id);
 
                         if (relEntityIds) {
@@ -1012,7 +1005,6 @@ BaseModelService.prototype = {
                             }
 
                             if (relEntityModel && relEntities) {
-                                console.log("relationshipModel entities:", JSON.stringify(relEntities));
                                 for (let rel of relationships) {
                                     if (rel.action || rel.attributes) {
                                         let relEntity = relEntities.find(v => v.id == rel.relTo.id);
