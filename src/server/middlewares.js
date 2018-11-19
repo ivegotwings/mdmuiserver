8202114f3c8b990fe67bcc4a406d61cd712568af
Ravi Kumar. Dhanasekaran <dhanasekaranr@Riversand.com>
2018-11-19 20:43:02 +0530

    Lint errors in middleware.

--- src/server/middlewares.js
+++ src/server/middlewares.js
@@ -6,22 +6,22 @@ function hasSql(value) {
     }
 
     // sql regex reference: http://www.symantec.com/connect/articles/detection-sql-injection-and-cross-site-scripting-attacks
-    var sql_meta = new RegExp('(%27)|(\')|(--)|(%23)|(#)', 'i');
+    let sql_meta = new RegExp('(%27)|(\')|(--)|(%23)|(#)', 'i');
     if (sql_meta.test(value)) {
         return true;
     }
 
-    var sql_meta2 = new RegExp('((%3D)|(=))[^\n]*((%27)|(\')|(--)|(%3B)|(;))', 'i');
+    let sql_meta2 = new RegExp('((%3D)|(=))[^\n]*((%27)|(\')|(--)|(%3B)|(;))', 'i');
     if (sql_meta2.test(value)) {
         return true;
     }
 
-    var sql_typical = new RegExp('w*((%27)|(\'))((%6F)|o|(%4F))((%72)|r|(%52))', 'i');
+    let sql_typical = new RegExp('w*((%27)|(\'))((%6F)|o|(%4F))((%72)|r|(%52))', 'i');
     if (sql_typical.test(value)) {
         return true;
     }
 
-    var sql_union = new RegExp('((%27)|(\'))union', 'i');
+    let sql_union = new RegExp('((%27)|(\'))union', 'i');
     if (sql_union.test(value)) {
         return true;
     }
@@ -31,7 +31,7 @@ function hasSql(value) {
 
 module.exports = {
     urlValidator: function(req, res, next){
-        var containsSql = false;
+        let containsSql = false;
 
         if (req.originalUrl !== null && req.originalUrl !== undefined) {
             if (hasSql(req.originalUrl) === true) {
