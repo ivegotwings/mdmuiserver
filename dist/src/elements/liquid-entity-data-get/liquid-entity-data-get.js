aee71b3d9480e560abbb87a5dbdee73d4c87d522
Shiv Kumar <shivshankerkumar@gmail.com>
2018-12-07 16:31:50 +0530

    p3: cherry-pick p3: legacy elements to converted p3

--- dist/src/elements/liquid-entity-data-get/liquid-entity-data-get.js
+++ dist/src/elements/liquid-entity-data-get/liquid-entity-data-get.js
@@ -8,36 +8,27 @@
   from HTML and may be out of place here. Review them and
   then delete this comment!
 */
-import '@polymer/polymer/polymer-legacy.js';
-
+import { PolymerElement } from '@polymer/polymer/polymer-element.js';
+import { mixinBehaviors } from '@polymer/polymer/lib/legacy/class.js';
 import '../liquid-dataobject-get-behavior/liquid-dataobject-get-behavior.js';
-import { Polymer as Polymer$0 } from '@polymer/polymer/lib/legacy/polymer-fn.js';
 
-Polymer$0({
-  _template: Polymer.html`
+class LiquidEntityDataGet
+    extends mixinBehaviors([
+      RUFBehaviors.LiquidDataObjectGetBehavior
+    ], PolymerElement) {
+  static get template() {
+    return Polymer.html`
+    `;
+  }
+  static get is() { return 'liquid-entity-data-get' }
 
-`,
+  constructor() {
+    super();
+  }
 
-  is: "liquid-entity-data-get",
-  behaviors: [RUFBehaviors.LiquidDataObjectGetBehavior],
-
-  /**
-* Content is not appearing - Content development is under progress.
-*/
-  attached: function () {
-  },
-
-  /**
-* Content is not appearing - Content development is under progress.
-*/
-  ready: function () {
-  },
-
-  properties: {
-      /**
-* <b><i>Content development is under progress... </b></i>
-*/
-      dataIndex: {
+  static get properties() {
+    return {
+        dataIndex: {
           type: String,
           value: "entityData"
       },
@@ -56,5 +47,8 @@ Polymer$0({
           type: String,
           value: "entities"
       }
+    }
   }
-});
+}
+
+customElements.define(LiquidEntityDataGet.is, LiquidEntityDataGet);
