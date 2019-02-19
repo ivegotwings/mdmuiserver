6320414460c2f88564f2017cc6fd6ccf76ac167e
Vishal Jariwala <vdjariwala@gmail.com>
2019-02-20 01:22:27 +0530

    fixed pebble-image-viewer memory leaks

--- src/elements/pebble-image-viewer/pebble-image-viewer.js
+++ src/elements/pebble-image-viewer/pebble-image-viewer.js
@@ -1,11 +1,10 @@
 import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
-import '../bedrock-lazy-load-behavior/bedrock-lazy-load-behavior.js';
-import { mixinBehaviors } from '@polymer/polymer/lib/legacy/class.js';
-class PebbleImageViewer extends mixinBehaviors([RUFBehaviors.LazyLoadBehavior], PolymerElement) {
+
+class PebbleImageViewer extends PolymerElement {
   static get template() {
-    return html`
+    return html `
     <style>
-      :host {
+      :host { 
         display: inline-block;
         overflow: hidden;
         position: relative;
@@ -85,6 +84,7 @@ class PebbleImageViewer extends mixinBehaviors([RUFBehaviors.LazyLoadBehavior], 
   static get is() {
     return "pebble-image-viewer";
   }
+
   static get properties() {
     return {
       /**
@@ -141,26 +141,68 @@ class PebbleImageViewer extends mixinBehaviors([RUFBehaviors.LazyLoadBehavior], 
       assetDetails: {
         type: Object,
         notify: true
+      },
+
+      callback: {
+        type: Object
+      },
+
+      active: {
+        type: Boolean,
+        value: false,
+        notify: true
+      },
+
+      lazyLoad: {
+        type: Boolean,
+        value: false
+      },
+
+      id: {
+        type: String,
+        observer: "_idChanged"
+      },
+      positionTrackerIntersectionObserver: {
+        type: Object
       }
     }
   }
+
   static get observers() {
     return [
       '_loadImage(src)'
     ]
   }
+
   constructor() {
     super();
     this._resolvedSrc = '';
   }
+
   disconnectedCallback() {
     super.disconnectedCallback();
+    this.removeEventListener('tap', this.enlargeImage);
+
+    this.positionTrackerIntersectionObserver = null;
+    this.callback = null;
   }
 
   connectedCallback() {
     super.connectedCallback();
+
+    if (this.lazyLoad) {
+      if (this.active) {
+        let detail = {
+          "target": this
+        };
+        this.callback(detail);
+      } else {
+        this._initLazyLoad();
+      }
+    }
     this.addEventListener('tap', this.enlargeImage);
   }
+
   _isImgHidden() {
     return !!this.sizing;
   }
@@ -175,11 +217,12 @@ class PebbleImageViewer extends mixinBehaviors([RUFBehaviors.LazyLoadBehavior], 
 
       sizedImg.style.backgroundImage = `url("${src}")`;
       sizedImg.style.backgroundSize = this.sizing;
-      if(src.includes("no-thumbnail")){
+      if (src.includes("no-thumbnail")) {
         sizedImg.style.cursor = "default";
       }
     }
   }
+
   _shapeChanged() {
     if (this.shape !== "circle") return;
 
@@ -188,12 +231,74 @@ class PebbleImageViewer extends mixinBehaviors([RUFBehaviors.LazyLoadBehavior], 
 
   enlargeImage() {
     let imageItem = this.assetDetails;
-    
-    if(imageItem){
-      this.dispatchEvent(new CustomEvent('enlarge-asset', {detail: {data: imageItem,name:"view-by-item"}, bubbles:true, composed:true}));
-    } else if(this.src !== "/src/images/no-thumbnail.svg"){
-        this.dispatchEvent(new CustomEvent('enlarge-asset', {detail: {data: this.src, name:"view-by-src"}, bubbles:true, composed:true}));
-    } 
+
+    if (imageItem) {
+      this.dispatchEvent(new CustomEvent('enlarge-asset', {
+        detail: {
+          data: imageItem,
+          name: "view-by-item"
+        },
+        bubbles: true,
+        composed: true
+      }));
+    } else if (this.src !== "/src/images/no-thumbnail.svg") {
+      this.dispatchEvent(new CustomEvent('enlarge-asset', {
+        detail: {
+          data: this.src,
+          name: "view-by-src"
+        },
+        bubbles: true,
+        composed: true
+      }));
+    }
+  }
+
+  _idChanged(newId, oldId) {
+    //console.log('id changed ', newId, oldId);
+    if (this.lazyLoad && oldId != undefined) {
+      if (this.active) {
+        let detail = {
+          "target": this
+        };
+        this.callback(detail);
+      } else {
+        this._initLazyLoad();
+      }
+    }
+  }
+
+  _initLazyLoad() {
+    //console.log('init lazy load called ', this.id);
+    if (!this.positionTrackerIntersectionObserver) {
+      this.positionTrackerIntersectionObserver = new IntersectionObserver(function (entries, observer) {
+        for (let i = 0; i < entries.length; i++) {
+          entries[i].target._lazyLoadCallback(entries[i]);
+        }
+      }, {});
+    }
+    if (this.active) {
+      let detail = {
+        "target": this
+      };
+      this.callback(detail);
+    } else {
+      this.positionTrackerIntersectionObserver.observe(this);
+    }
+  }
+
+  _lazyLoadCallback(e) {
+    if (e.isIntersecting || e.intersectionRatio > 0) {
+      //console.log('lazy load callback ', this.id);
+      this.active = true;
+      this.positionTrackerIntersectionObserver.unobserve(this);
+      if (this.callback) {
+        let detail = {
+          "target": this
+        };
+        this.callback(detail);
+      }
+    }
   }
 }
+
 customElements.define(PebbleImageViewer.is, PebbleImageViewer);
