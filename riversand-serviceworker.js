445e9e5d0d054f86f008cf9d8f0241c69885c21a
Shiv Kumar <shivshankerkumar@gmail.com>
2019-02-27 13:29:56 +0530

    404 error page integration

--- riversand-serviceworker.js
+++ riversand-serviceworker.js
@@ -1,9 +1,9 @@
 const PRECACHE_URLS = [
   '/error-page/error-page.html',
-  '/error-page/css/coming-soon.min.css', // Alias for index.html
-  '/error-page/vendor/bootstrap/css/bootstrap.min.css',
-  '/error-page/img/bg-mobile-fallback.jpg',
-  '/error-page/img/riversandlogo.svg'
+  '/error-page/coming-soon.min.css',
+  '/error-page/bootstrap.min.css',
+  '/error-page/bg-mobile-fallback.jpg',
+  '/error-page/riversandlogo.svg'
 ];
 const PRECACHE = 'precache-v1';
 
@@ -12,26 +12,25 @@ self.addEventListener('install', function(event) {
       caches.open(PRECACHE)
         .then(cache => cache.addAll(PRECACHE_URLS))
         .then(self.skipWaiting())
-    );
-  });
+    );  
+});
   
 
 self.handleResponse = async function(request, clientId){
   const response = await fetch(request);
-  if((response.status == 401)){
+  if((response.status == 401 || response.status == 404)){
     if (clientId){
         // Get the client.
         const client = await self.clients.get(clientId);
         // Exit early if we don't get the client. Eg, if it closed.
         if (client){
             client.postMessage({
-              msg: "AUTHENTICATION_ERROR",
               status: response.status,
               url: request.url
           });
         }
       }
-      caches.match(event.request).then(cachedResponse => {
+      caches.match(request).then(cachedResponse => {
         if (cachedResponse) {
           return cachedResponse;
         }
